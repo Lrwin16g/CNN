8393c80347c45fea8feba8664d9523d86dd322a1
Yuzuru.N <ynakamura@YuzuruNnoMacBook-Pro.local>
2019-11-24 01:04:09 +0900

    update

--- conv2d.h
+++ conv2d.h
@@ -13,6 +13,39 @@ public:
                  const double *bias,
                  double ****output);
     void backward(double const * const * const * const *dout, double ****dx);
-}
+
+private:
+    int batch_size_;
+    int channel_;
+    int height_;
+    int width_;
+    int filter_num_;
+    int filter_h_;
+    int filter_w_;
+    int stride_;
+    int pad_;
+    int output_h_;
+    int output_w_;
+    int input_col_h_;
+    int input_col_w_;
+    int weight_col_h_;
+    int weight_col_w_;
+    int tmp_col_h_;
+    int tmp_col_w_;
+    int output_col_h_;
+    int output_col_w_;
+
+    double **input_col_;
+    double **input_col_T_;
+    double **dx_col_;
+    double **weight_col_;
+    double **weight_col_T_;
+    double **tmp_col_;
+    double **output_col_;
+    double **dout_col_;
+    double ****d_weight_;
+    double *d_bias_;
+    double **d_weight_col_;
+};
 
 #endif
