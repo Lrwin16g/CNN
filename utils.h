ac3d36e1d4c66d107bf1dcbf06ad7cdbcc2965e4
Yuzuru.N <ynakamura@YuzuruNnoMacBook-Pro.local>
2019-11-24 01:04:20 +0900

    update

--- utils.h
+++ utils.h
@@ -3,11 +3,18 @@
 
 namespace util
 {
+    double* alloc(int dim_1);
     double** alloc(int dim_1, int dim_2);
     double**** alloc(int dim_1, int dim_2, int dim_3, int dim_4);
 
+    void free(double *data);
     void free(double **data, int dim_1);
     void free(double ****data, int dim_1, int dim_2, int dim_3);
+
+    void transpose(double const * const *src, double **dst, int rows, int cols);
+
+    void dot(double const * const *lhs, double const * const *rhs, double **dst,
+             int lhs_rows, int lhs_cols, int rhs_rows, int rhs_cols);
 
     void im2col(double const * const * const * const *src, double **dst,
                 int batch_size, int channel, int height, int width,
