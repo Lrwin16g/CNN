c44f75bb4909c4cff1f91a7256e0820839dda0a4
Yuzuru.N <ynakamura@YuzuruNnoMacBook-Pro.local>
2019-11-24 21:51:26 +0900

    update

--- conv2d.cpp
+++ conv2d.cpp
@@ -1,8 +1,6 @@
 #include "conv2d.h"
 #include "utils.h"
 
-#include <cmath>
-
 Conv2d::Conv2d(int batch_size, int channel, int height, int width,
                int filter_num, int filter_h, int filter_w, int stride, int pad)
     : batch_size_(batch_size), channel_(channel), height_(height), width_(width),
@@ -21,33 +19,33 @@ Conv2d::Conv2d(int batch_size, int channel, int height, int width,
     // 入力画像を2次元配列に展開した時のサイズ
     input_col_h_ = batch_size_ * output_h_ * output_w_;
     input_col_w_ = channel_ * filter_h_ * filter_w_;
-    input_col_ = util::alloc(input_col_h_, input_col_w_);
-    input_col_T_ = util::alloc(input_col_w_, input_col_h_);
-    dx_col_ = util::alloc(input_col_h_, input_col_w_);
+    input_col_ = util::alloc<double>(input_col_h_, input_col_w_);
+    input_col_T_ = util::alloc<double>(input_col_w_, input_col_h_);
+    dx_col_ = util::alloc<double>(input_col_h_, input_col_w_);
 
     // 重みを2次元配列に展開した時のサイズ
     weight_col_h_ = channel_ * filter_h_ * filter_w_;
     weight_col_w_ = filter_num_;
-    weight_col_ = util::alloc(weight_col_h_, weight_col_w_);
-    weight_col_T_ = util::alloc(weight_col_w_, weight_col_h_);
+    weight_col_ = util::alloc<double>(weight_col_h_, weight_col_w_);
+    weight_col_T_ = util::alloc<double>(weight_col_w_, weight_col_h_);
 
     // 入力と重みの積を格納する行列
     tmp_col_h_ = input_col_h_;
     tmp_col_w_ = weight_col_w_;
-    tmp_col_ = util::alloc(tmp_col_h_, tmp_col_w_);
+    tmp_col_ = util::alloc<double>(tmp_col_h_, tmp_col_w_);
 
     // 出力の行列形式
     output_col_h_ = input_col_h_;
     output_col_w_ = weight_col_w_;
-    output_col_ = util::alloc(output_col_h_, output_col_w_);
-    dout_col_ = util::alloc(output_col_h_, output_col_w_);
+    output_col_ = util::alloc<double>(output_col_h_, output_col_w_);
+    dout_col_ = util::alloc<double>(output_col_h_, output_col_w_);
 
     // 重み・バイアスの勾配
-    d_weight_ = util::alloc(filter_num_, channel_, filter_h_, filter_w_);
-    d_bias_ = util::alloc(filter_num_);
+    d_weight_ = util::alloc<double>(filter_num_, channel_, filter_h_, filter_w_);
+    d_bias_ = util::alloc<double>(filter_num_);
 
     // 重みの勾配の行列形式
-    d_weight_col_ = util::alloc(weight_col_h_, weight_col_w_);
+    d_weight_col_ = util::alloc<double>(weight_col_h_, weight_col_w_);
 }
 
 Conv2d::~Conv2d()
@@ -71,8 +69,7 @@ void Conv2d::forward(double const * const * const * const *input,
                      double ****output)
 {
     // 入力を2次元配列に展開
-    util::im2col(input, input_col_, batch_size_, channel_, height_, width_,
-                 filter_h_, filter_w_, stride_, pad_);
+    im2col(input, input_col_);
 
     // 重みを2次元配列に展開
     for (int n = 0; n < filter_num_; ++n) {
@@ -164,6 +161,62 @@ void Conv2d::backward(double const * const * const * const *dout, double ****dx)
               weight_col_w_, weight_col_h_);
 
     // 行列をテンソルに戻す
-    util::col2im(dx_col_, dx, batch_size_, channel_, height_, width_,
-                 filter_h_, filter_w_, stride_, pad_);
+    col2im(dx_col_, dx);
+}
+
+void Conv2d::im2col(double const * const * const * const *src, double **dst)
+{
+    for (int n = 0; n < batch_size_; ++n) {
+        for (int c = 0; c < channel_; ++c) {
+            for (int y = -pad_; y <= height_ + pad_ - filter_h_; y += stride_) {
+                for (int x = -pad_; x <= width_ + pad_ - filter_w_; x += stride_) {
+                    int dst_y = n * (output_h_ * output_w_) + ((y + pad_) / stride_) * output_w_
+                                + ((x + pad_) / stride_);
+                    for (int v = 0; v < filter_h_; ++v) {
+                        for (int u = 0; u < filter_w_; ++u) {
+                            int src_y = y + v;
+                            int src_x = x + u;
+                            int dst_x = c * (filter_h_ * filter_w_) + v * filter_w_ + u;
+
+                            if (0 <= src_y && src_y < height_ && 0 <= src_x && src_x < width_) {
+                                dst[dst_y][dst_x] = src[n][c][src_y][src_x];
+                            } else {
+                                dst[dst_y][dst_x] = 0.0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void Conv2d::col2im(double const * const *src, double ****dst)
+{
+    for (int n = 0; n < batch_size_; ++n) {
+        for (int c = 0; c < channel_; ++c) {
+            for (int y = 0; y < height_; ++y) {
+                for (int x = 0; x < width_; ++x) {
+                    dst[n][c][y][x] = 0.0;
+                }
+            }
+            for (int y = -pad_; y <= height_ + pad_ - filter_h_; y += stride_) {
+                for (int x = -pad_; x <= width_ + pad_ - filter_w_; x += stride_) {
+                    int src_y = n * (output_h_ * output_w_) + ((y + pad_) / stride_) * output_w_
+                                + ((x + pad_) / stride_);
+                    for (int v = 0; v < filter_h_; ++v) {
+                        for (int u = 0; u < filter_w_; ++u) {
+                            int src_x = c * (filter_h_ * filter_w_) + v * filter_w_ + u;
+                            int dst_y = y + v;
+                            int dst_x = x + u;
+
+                            if (0 <= dst_y && dst_y < height_ && 0 <= dst_x && dst_x < width_) {
+                                dst[n][c][dst_y][dst_x] += src[src_y][src_x];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
